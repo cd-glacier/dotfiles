1728f8cdbcb135d6e91e9ca39934ac55a0914aee
g-hyoga <hyoga0216@gmail.com>
2018-03-01 20:06:05 +0900

    modify git pre func

--- .config/fish/functions/ga.fish
+++ .config/fish/functions/ga.fish
@@ -1,4 +1,26 @@
+
+function pre_git_add
+  echo pre
+  switch (echo $USER)
+  case glacier
+
+  case '*'
+    echo format java
+    ./bin/mvn.sh formatter:format
+
+    echo creating gitcommit_message file 
+    set branch (git_branch)
+    set list (string split -m2 - $branch)
+    set commit_template "$list[1]-$list[2]-"
+    touch "$GITCOMMIT_MESSAGE_PATH/$GITCOMMIT_MESSAGE_FILE"
+    echo "$commit_template" > "$GITCOMMIT_MESSAGE_PATH/$GITCOMMIT_MESSAGE_FILE"
+  end
+end
+
 function ga
+  echo "pre_git_add"
+  pre_git_add
+
   echo git add .
   git add .
 end
--- .config/fish/functions/gco.fish
+++ .config/fish/functions/gco.fish
@@ -1,23 +1,4 @@
-function pre_git_commit
-  echo pre
-  switch (echo $USER)
-  case glacier
-
-  case '*'
-    echo format java
-    ./bin/mvn.sh formatter:format
-
-    echo creating gitcommit_message file 
-    set branch (git_branch)
-    set list (string split -m2 - $branch)
-    set commit_template "$list[1]-$list[2]-"
-    touch "$GITCOMMIT_MESSAGE_PATH/$GITCOMMIT_MESSAGE_FILE"
-    echo "$commit_template" > "$GITCOMMIT_MESSAGE_PATH/$GITCOMMIT_MESSAGE_FILE"
-  end
-end
-
 function gco $argv
-  pre_git_commit
   echo git commit
   git commit $argv
 end
