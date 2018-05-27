379362a23e7f57560ef9fa22ed75ea69b71191ac
g-hyoga <hyoga0216@gmail.com>
2018-05-27 22:19:35 +0900

    delete unnecessary env

--- common/.config/fish/config.fish
+++ common/.config/fish/config.fish
@@ -14,20 +14,6 @@ set PATH $PATH $GOPATH/bin
 #set GITCOMMIT_MESSAGE_PATH ~
 #git config --global commit.template "$GITCOMMIT_MESSAGE_PATH/$GITCOMMIT_MESSAGE_FILE"
 
-# web assembly
-set PATH $PATH /Users/$USER/Dropbox/dev/src/github.com/juj/emsdk/clang/fastcomp/build_incoming_64/bin
-set PATH $PATH /Users/$USER/Dropbox/dev/src/github.com/juj/emsdk/node/8.9.1_64bit/bin
-set PATH $PATH /Users/$USER/Dropbox/dev/src/github.com/juj/emsdk/emscripten/incoming
-set PATH $PATH /Users/$USER/Dropbox/dev/src/github.com/juj/emsdk/binaryen/master_64bit_binaryen/bin
-set -g EMSDK /Users/$USER/Dropbox/dev/src/github.com/juj/emsdk
-#set -g EM_CONFIG /Users/$USER/.emscripten
-set -g EMSCRIPTEN /Users/$USER/Dropbox/dev/src/github.com/juj/emsdk/emscripten/incoming
-set -g BINARYEN_ROOT /Users/$USER/Dropbox/dev/src/github.com/juj/emsdk/binaryen/master_64bit_binaryen
-set PATH $PATH /Users/$USER/Dropbox/dev/src/github.com/WebAssembly/wabt/bin
-set LLVM_ROOT -g /Users/$USER/Dropbox/dev/src/github.com/juj/emsdk/clang/fastcomp/build_incoming_64/bin
-set NODE_JS -g /Users/$USER/Dropbox/dev/src/github.com/juj/emsdk/node/8.9.1_64bit/bin/node
-set EMSCRIPTEN_ROOT -g /Users/$USER/Dropbox/dev/src/github.com/juj/emsdk/emscripten/incoming
-
 # vi modeではなんか[I]みたいなの出るからオーバーライド
 function fish_mode_prompt 
 end
