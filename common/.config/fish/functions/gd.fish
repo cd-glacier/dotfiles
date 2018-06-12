3868d62310d05c46345dfecf5fc1b730a6f581ec
g-hyoga <hyoga0216@gmail.com>
2018-06-12 18:46:26 +0900

    update gd command You can select file which you want to see

--- common/.config/fish/functions/gd.fish
+++ common/.config/fish/functions/gd.fish
@@ -2,14 +2,19 @@
 function gd
   git log --oneline | peco | read first_commit
   echo "selected: $first_commit"
-  sleep 0.4
+  # 速すぎると選択した感がなかった
+  sleep 0.3
   git log --oneline | peco | read second_commit
   echo "selected: $second_commit"
 
   set first_commit_id (string split -m1 ' ' $first_commit)
-
   set second_commit_id (string split -m1 ' ' $second_commit)
 
-  git diff $first_commit_id[1] $second_commit_id[1]
+  git diff --name-only $first_commit_id[1] $second_commit_id[1] \
+  | peco \
+  | read selected_file
+  
+  git diff $first_commit_id[1] $second_commit_id[1] -- $selected_file
 end
+
 
