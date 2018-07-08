7e1666bcf65130247e31ae3c14afac6f8e8022e8
g-hyoga <hyoga0216@gmail.com>
2018-07-08 17:42:07 +0900

    Modify gp function that communicate when branch is master.

--- common/.config/fish/functions/gp.fish
+++ common/.config/fish/functions/gp.fish
@@ -1,4 +1,11 @@
 function gp
   echo git push origin (git_branch)
-  git push origin (git_branch)
+  if [ (git_branch) = "master" ]
+   read --prompt="echo 'Your branch is master. push origin master? (Y/n):'" yn
+   if [ $yn = "Y" ] 
+    git push origin (git_branch)
+   end
+  else 
+    git push origin (git_branch)
+  end
 end
