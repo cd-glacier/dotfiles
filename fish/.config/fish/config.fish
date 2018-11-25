178da43252cb1a0d7f163c7478406037717da7d6
g-hyoga <hyoga0216@gmail.com>
2018-11-26 00:46:32 +0900

    Change prompt

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -11,35 +11,27 @@ fish_vi_key_bindings
 
 ## prompt
 function fish_prompt
-  if [ $status -eq 0 ]
-    set fish_face ">><(( o> "
+  set u (string split -r -m2 '/' (pwd))
+	set branch_status $u[2]:(git_branch)
+
+  set_color red
+  echo -n ❱ ""
+	set_color yellow
+  if test -z $branch_status
   else
-    set fish_face ">><(( x> "
+    echo -n $branch_status ❱ ""
   end
+	set_color green
+  echo -n $fish_bind_mode ❱ ""
+	set_color blue
+  echo (prompt_pwd)
 
-	if test "$fish_key_bindings" = "fish_vi_key_bindings"
-		switch $fish_bind_mode
-		case default
-			set_color --bold red white
-		case insert
-			set_color --bold green white
-		case replace-one
-			set_color --bold green white
-		case visual
-			set_color --bold magenta white
-		end
-  end
-
-  echo (prompt_pwd) $fish_face
+	set_color magenta
+  echo ❯❯❯ ""
 end
 
 #gitのbranch名の抽出
 function git_branch
 	git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
-end
-
-#右prompt
-function fish_right_prompt
-	echo [(git_branch)]
 end
 
