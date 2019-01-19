9367874ba7662feb58312d7ce84b77c7358b1ccc
g-hyoga <hyoga0216@gmail.com>
2019-01-20 04:42:39 +0900

    Modify prompt: use __fish_git_prompt

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,20 +9,26 @@ fish_vi_key_bindings
 
 ##### prompt #####
 
+## __fish_git_prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_show_informative_status 'yes'
+
 ## prompt
 function fish_prompt
-  set u (string split -r -m2 '/' (pwd))
-	set branch_status $u[2]:(git_branch)
+	set git_user (git config --get user.name)
 
   set_color red
   echo -n ❱ ""
 	set_color yellow
-  if test -z $branch_status
+  if test -z $git_user
   else
-    echo -n $branch_status ❱ ""
+    echo -n $git_user ❱ ""
   end
 	set_color green
-  echo -n $fish_bind_mode ❱ ""
+  echo -n (__fish_git_prompt "%s") ❱ ""
 	set_color blue
   echo (prompt_pwd)
 
@@ -30,8 +36,4 @@ function fish_prompt
   echo ❯❯❯ ""
 end
 
-#gitのbranch名の抽出
-function git_branch
-	git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
-end
 
