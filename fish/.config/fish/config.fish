a57425d7c533e8a7ddae5b606d2db74fee6dffaa
g-hyoga <hyoga0216@gmail.com>
2019-01-27 18:05:19 +0900

    Refacotoring fish_prompt

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -18,27 +18,30 @@ set __fish_git_prompt_show_informative_status 'yes'
 
 ## prompt
 function fish_prompt
-	set git_user (git config --get user.name)
+  set git_user (git config --get user.name)
+  set git_prompt (__fish_git_prompt "%s")
   set columns (tput cols)
 
-  set git_user_len (string length git_user)
-  set git_prompt_len (string length (__fish_git_prompt "%s"))
+  set git_user_len (string length "$git_user")
+  set git_prompt_len (string length "$git_prompt")
   set prompt_pwd_len (string length (prompt_pwd))
   set other_len (string length "❱ ❱ ❱ ")
   set prompt_len (math $git_user_len + $git_prompt_len + $prompt_pwd_len + $other_len)
 
-  if [ $columns -gt $prompt_len ] # ❱ g-hyoga ❱ master|✔ ❱ ~/D/d/s/g/g/dotfiles
-    printf "%s%s " (set_color red) "❱"
+  printf "%s%s " (set_color red) "❱"
+
+  if [ $columns -gt $prompt_len ]
+    # ❱ g-hyoga ❱ master|✔ ❱ ~/D/d/s/g/g/dotfiles
     printf "%s%s %s " (set_color yellow) $git_user "❱"
     printf "%s%s %s " (set_color green) (__fish_git_prompt "%s") "❱"
     printf "%s%s\n" (set_color blue) (prompt_pwd)
-  else if [ $columns -lt $prompt_len -a $columns -gt (math $prompt_len - $git_user_len) ] # ❱ ❱ master|✔ ❱ ~/D/d/s/g/g/dotfiles
-    printf "%s%s " (set_color red) "❱"
+  else if [ $columns -le $prompt_len -a $columns -ge (math $prompt_len - $git_user_len) ]
+    # ❱ ❱ master|✔ ❱ ~/D/d/s/g/g/dotfiles
     printf "%s%s " (set_color yellow) "❱"
     printf "%s%s %s " (set_color green) (__fish_git_prompt "%s") "❱"
     printf "%s%s\n" (set_color blue) (prompt_pwd)
-  else if [ $columns -lt (math $prompt_len - $git_user_len) -a $columns -gt (math $prompt_len - $git_user_len - $git_prompt_len) ] # ❱ ❱ ❱ ~/D/d/s/g/g/dotfiles
-    printf "%s%s " (set_color red) "❱"
+  else if [ $columns -le (math $prompt_len - $git_user_len) -a $columns -ge (math $prompt_len - $git_user_len - $git_prompt_len) ]
+    # ❱ ❱ ❱ ~/D/d/s/g/g/dotfiles
     printf "%s%s " (set_color yellow) "❱"
     printf "%s%s " (set_color green) "❱"
     printf "%s%s\n" (set_color blue) (prompt_pwd)
