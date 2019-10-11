7a1f1caf503f260e451d8bac94bc948e1fa1b34b
g-hyoga <hyoga0216@gmail.com>
2019-10-11 14:59:08 +0900

    Add grpc-call command

---
+++ tools/bin/installer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+tools=(grpc-call)
+
+for file in "${tools[@]}"; do
+  echo "link $file to /usr/local/bin"
+	ln -sf "$(PWD)/tools/$file" "/usr/local/bin/$(basename $file)"
+
+  echo "link completions"
+  ln -sf "$(PWD)/tools/completions/$file.fish" "$HOME/.config/fish/completions/$file.fish"
+done
+
+echo 'done'
---
+++ tools/completions/grpc-call.fish
@@ -0,0 +1,36 @@
+
+complete --no-files \
+  --command grpc-call \
+  --long-option "host" \
+  --description "--host=localhost:8080"
+
+complete --no-files \
+  --command grpc-call \
+  --long-option "proto-root" \
+  --description "--proto-root=./proto"
+
+complete --no-files \
+  --command grpc-call \
+  --long-option "package" \
+  --description "package name. --package=hoge.services.v1"
+
+complete --no-files \
+  --command grpc-call \
+  --long-option "service" \
+  --description "service name"
+
+complete --no-files \
+  --command grpc-call \
+  --long-option "method" \
+  --description "you are calling method. --method=ListHoges"
+
+complete --no-files \
+  --command grpc-call \
+  --long-option "meta" \
+  --description "metadata. --meta=user_id:123,hoge:foo"
+
+complete --no-files \
+  --command grpc-call \
+  --long-option "message" \
+  --description "message. --message='{user_id: 1234}'"
+
