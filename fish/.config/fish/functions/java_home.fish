51cbddeb8d8dc7e7aefcfb0747dbe5241d9e3acd
g-hyoga <hyoga0216@gmail.com>
2019-10-19 15:35:26 +0900

    Improve java_home command

--- fish/.config/fish/functions/java_home.fish
+++ fish/.config/fish/functions/java_home.fish
@@ -1,8 +1,23 @@
 
+
 function java_home
-  # /usr/libexec/java_home -V
-  set JAVA_HOME (/usr/libexec/java_home -v "$argv")
+  if [ "$argv" != "" ]
+    set selected_java_version "$argv"
+  else
+    set java_list '/tmp/java_home'
+
+    /usr/libexec/java_home -V 2> /tmp/tmp
+    tail -n+2 /tmp/tmp \
+      | sed 's/^ *//g' \
+      | sed 's/	/ /g' \
+      | sed 's/,//g' \
+      > "$java_list"
+
+    set selected_java_version (cat "$java_list" | fzf | awk '{print $1}')
+  end
+
+  set JAVA_HOME (/usr/libexec/java_home -v "$selected_java_version")
   set -x PATH $JAVA_HOME/bin $PATH
-  echo $JAVA_HOME
+  echo "change to $JAVA_HOME"
 end
 
