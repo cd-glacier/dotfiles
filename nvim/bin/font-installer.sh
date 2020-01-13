bc3e619152997996b92d0754a10b6bf468262672
g-hyoga <hyoga0216@gmail.com>
2020-01-13 16:02:02 +0900

    Introduce vim devicons

--- nvim/bin/font-installer.sh
+++ nvim/bin/font-installer.sh
@@ -3,7 +3,7 @@
 echo "##### install font #####"
 
 echo "----- install FiraCode -----"
-if [ -z "$(ls ~/Library/Fonts/ | grep firacode)" ]; then
+if [ -z "$(ls ~/Library/Fonts/ | grep FiraCode)" ]; then
   echo "FiraCode already exists"
 else
   git clone git@github.com:tonsky/FiraCode.git
@@ -12,7 +12,7 @@ else
 fi
 
 echo "----- install PowerLine fonts -----"
-if [ -z "$(ls ~/Library/Fonts/ | grep powerline)" ]; then
+if [ -z "$(ls ~/Library/Fonts/ | grep PowerLine)" ]; then
   git clone https://github.com/powerline/fonts.git --depth=1
   cd fonts
   ./install.sh
@@ -20,4 +20,12 @@ if [ -z "$(ls ~/Library/Fonts/ | grep powerline)" ]; then
   rm -rf fonts
 else
   echo "PoewrLine fonts already exists"
+fi
+
+echo "----- install nerd-fonts -----"
+if [ -z "$(ls ~/Library/Fonts/ | grep NerdFonts)" ]; then
+  brew tap homebrew/cask-fonts
+  brew cask install font-hack-nerd-font
+else
+  echo "NerdFonts already exists"
 fi
