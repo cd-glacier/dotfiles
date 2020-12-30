b9a37a29194df3add456f6bce935b8471d0c7e9e
g-hyoga <hyoga0216@gmail.com>
2020-12-31 00:56:06 +0900

    Add starship.toml

--- bash/bin/installer.sh
+++ bash/bin/installer.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
 echo "##### link bash setting files ######"
-LINK_FILES=(.bash_profile .bashrc)
+LINK_FILES=(.bash_profile .bashrc .config/starship.toml)
 
 for file in ${LINK_FILES[@]}; do \
   unlink ~/$file&>/dev/null
