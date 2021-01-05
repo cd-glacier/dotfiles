6a32f5c36360d4c1c1520c9f886e5a42f26d7541
g-hyoga <hyoga0216@gmail.com>
2021-01-05 20:12:52 +0900

    Add issue viewer

--- tools/bin/installer.sh
+++ tools/bin/installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tools=(grpc-call memo ghe-noti co pr add push preview-git-file xcode studio rubymine)
+tools=(grpc-call memo ghe-noti co pr issue add push preview-git-file xcode studio rubymine)
 
 for file in "${tools[@]}"; do
   echo "link $file to /usr/local/bin"
