1d3e4a9cf0fd946cddb9c2638d0d9f7b1295ec9f
g-hyoga <hyoga0216@gmail.com>
2021-02-03 17:23:03 +0900

    bash vim mode and setting prompt

--- bash/bin/installer.sh
+++ bash/bin/installer.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
 echo "##### link bash setting files ######"
-LINK_FILES=(.bash_profile .bashrc .config/starship.toml)
+LINK_FILES=(.bash_profile .bashrc .config/starship.toml .inputrc)
 
 for file in ${LINK_FILES[@]}; do \
   unlink ~/$file&>/dev/null
