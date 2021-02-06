297e034e6b2034268516ad3e49e1ff7b444af0bc
g-hyoga <hyoga0216@gmail.com>
2021-02-06 09:36:53 +0900

    Move bash installer

--- bash/bin/installer.sh
+++ bash/bin/installer.sh
@@ -1,5 +1,25 @@
 #!bin/bash
 
+install_command() {
+  echo "----- install $1 -----"
+  if [ -z $(which $1) ]; then
+    brew install $1
+  fi
+}
+
+echo '#### install with brew'
+
+install_command tmux
+install_command ghq
+install_command gh
+install_command bat
+install_command noti
+install_command fd
+install_command fx
+install_command bash
+install_command starship
+install_command reattach-to-user-namespace
+
 echo "##### link bash setting files ######"
 LINK_FILES=(.bash_profile .bashrc .config/starship.toml .inputrc)
 
