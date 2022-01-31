3cfe743040e979a2f3177a898c28c40df094ddc1
cd-glacier <hyoga0216@gmail.com>
2022-01-31 14:53:28 +0900

    Add mypr

--- tools/bin/installer.sh
+++ tools/bin/installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tools=(grpc-call memo ghe-noti co pr issue add push preview-git-file xcode studio rubymine idea java-home idea)
+tools=(grpc-call memo ghe-noti co pr issue add push preview-git-file xcode studio rubymine idea java-home idea mypr)
 
 for file in "${tools[@]}"; do
   echo "link $file to /usr/local/bin"
