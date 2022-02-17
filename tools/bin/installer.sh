9086d940e96c20f1fc8bb92359cca430ccc24fcb
cd-glacier <hyoga0216@gmail.com>
2022-02-17 14:04:51 +0900

    Combine mypr and prmypr

--- tools/bin/installer.sh
+++ tools/bin/installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tools=(grpc-call memo ghe-noti co pr issue add push preview-git-file xcode studio rubymine idea java-home idea mypr greset)
+tools=(grpc-call memo ghe-noti co pr issue add push preview-git-file xcode studio rubymine idea java-home idea greset)
 
 for file in "${tools[@]}"; do
   echo "link $file to /usr/local/bin"
