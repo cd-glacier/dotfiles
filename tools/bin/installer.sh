c52764353b63f4ea45c1e8e7acf9d8f859678a6d
cd-glacier <hyoga0216@gmail.com>
2023-02-03 17:49:52 +0900

    Replce gh extension

--- tools/bin/installer.sh
+++ tools/bin/installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tools=(grpc-call ghe-noti pr add push preview-git-file java-home)
+tools=(grpc-call ghe-noti add push preview-git-file java-home)
 
 for file in "${tools[@]}"; do
   echo "link $file to /usr/local/bin"
