23f6ec62ae6d4867feeff7f2401c3011896869c7
cd-glacier <hyoga0216@gmail.com>
2023-03-17 11:28:17 +0900

    Change github notification icon

--- sketchybar/.config/sketchybar/plugins/ghe_notification.sh
+++ sketchybar/.config/sketchybar/plugins/ghe_notification.sh
@@ -24,25 +24,9 @@ notifications=$(curl -s -u "$USER_NAME:$ACCESS_TOKEN" \
 
 notifications_length=$(echo $notifications | jq '. | length')
 
-bar_text() {
-  if [ $1 -gt 20 ]; then
-    sketchybar --set $NAME icon="󰈸" label="$notifications_length"
-  elif [ $1 -gt 10 ]; then
-    sketchybar --set $NAME icon="󰖓" label="$notifications_length"
-  elif [ $1 -gt 5 ]; then
-    sketchybar --set $NAME icon="" label="$notifications_length"
-  else
-    sketchybar --set $NAME icon="󰀸" label="$notifications_length"
-  fi
-}
-
 if [[ ! ${notifications_length} =~  ^[0-9]+$ ]]; then
   sketchybar --set $NAME label="Failed to connect to GHE"
 fi
 
-if [[ "$notifications_length" > "0" ]]; then
-  bar_text "$notifications_length"
-else
-  sketchybar --set $NAME icon="󰖨" label="0"
-fi
+sketchybar --set $NAME icon="" label="$notifications_length"
 
