df649cf8dfb6229205e983b8e599aaeb8414ad20
cd-glacier <hyoga0216@gmail.com>
2023-03-24 22:46:10 +0900

    Update sketchybar

--- sketchybar/.config/sketchybar/plugins/clock.sh
+++ sketchybar/.config/sketchybar/plugins/clock.sh
@@ -4,4 +4,4 @@
 # the item invoking this script:
 # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
 
-sketchybar --set $NAME label="$(date '+%m/%d %H:%M')"
+sketchybar --set $NAME label="$(date '+%m/%d %a %H:%M')"
