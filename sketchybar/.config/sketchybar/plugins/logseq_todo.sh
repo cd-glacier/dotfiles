991990cbf99db885cae306d56ef3a46fef9f438a
cd-glacier <hyoga0216@gmail.com>
2023-06-23 18:17:42 +0900

    Fix space

--- sketchybar/.config/sketchybar/plugins/logseq_todo.sh
+++ sketchybar/.config/sketchybar/plugins/logseq_todo.sh
@@ -4,7 +4,7 @@ journals_directory="/Users/hyoga-yoshida/glacier.in.antarctica@gmail.com - Googl
 today=$(date "+%Y_%m_%d")
 today_jounal="$journals_directory/$today.md"
 
-last_todo=$(grep "TODO" "$today_jounal" | tail -n 1 | sed 's/- TODO //g')
+last_todo=$(grep "TODO" "$today_jounal" | tail -n 1 | awk '{print $3}')
 
 sketchybar --set $NAME icon="" label="$last_todo"
 
