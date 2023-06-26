ca517cc7b3261b5b78e910d03967586826d793f7
cd-glacier <hyoga0216@gmail.com>
2023-06-26 10:02:52 +0900

    Update logseq_todo

--- sketchybar/.config/sketchybar/plugins/logseq_todo.sh
+++ sketchybar/.config/sketchybar/plugins/logseq_todo.sh
@@ -6,5 +6,9 @@ today_jounal="$journals_directory/$today.md"
 
 last_todo=$(grep "TODO" "$today_jounal" | tail -n 1 | awk '{print $3}')
 
+if [[ -z "$last_todo" ]]; then
+  last_todo="nothing"
+fi
+
 sketchybar --set $NAME icon="" label="$last_todo"
 
