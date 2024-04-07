7f755ed0f8a0e9997ae5b8707a8bdd4683e43791
cd-glacier <hyoga0216@gmail.com>
2024-04-07 12:05:26 +0900

    Fix adb_device

--- sketchybar/.config/sketchybar/plugins/adb_device.sh
+++ sketchybar/.config/sketchybar/plugins/adb_device.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-label=$(adb devices -l | grep -o -E 'model:\s*\S*' | tail -n 1 | sed 's/model://')
+device=$(adb devices -l | grep -o -E 'model:\s*\S*' | tail -n 1 | sed 's/model://')
+
+if [[ ! -z "$device" ]]; then
+  label="Connected $device"
+else
+  label="Not Connected"
+fi
 
 sketchybar --set $NAME label="$label"
 
